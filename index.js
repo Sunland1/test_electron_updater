65a9fe38926b1a2e225e27aa31ad093095173fb4
HORVILLE Alexandre <ahorville@MDNADMIN.local>
2021-08-19 08:33:58 +0200

    UPDATE

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {autoUpdater} = require('electron-updater');
 
 
 let mainWindow;
+let secondWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -10,16 +11,35 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-    },
+      contextIsolation: false,
+  }
   });
   mainWindow.loadFile('index.html');
 
   mainWindow.on('closed', function () {
     mainWindow = null;
+    createSecondWindow()
   });
+
 
   mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
+  });
+
+}
+
+function createSecondWindow(){
+  secondWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+  }
+  });
+  secondWindow.loadFile('second.html');
+  secondWindow.on('closed', function () {
+    secondWindow = null;
   });
 }
 
@@ -37,6 +57,9 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
+  else if (secondWindow === null) {
+    createSecondWindow();
+  }
 });
 
 ipcMain.on('app_version', (event) => {
@@ -46,14 +69,21 @@ ipcMain.on('app_version', (event) => {
 
 ipcMain.on('restart_app', () => {
     autoUpdater.quitAndInstall();
-  });
+});
+
 
 
 
 autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
+  mainWindow.webContents.send('update_available');
+});
 
 autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
+  mainWindow.webContents.send('update_downloaded');
+});
+
+
+autoUpdater.on("update-not-available" , () => {
+  mainWindow.webContents.send('update_not_available')
+})
+
